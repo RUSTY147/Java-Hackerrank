44ffdf9a668756a475364c6f98b482f22190625f
K.V.R.L.KRISHNA AJAY <70434827+RUSTY147@users.noreply.github.com>
2021-12-18 17:28:44 +0530

    Add files via upload

---
+++ Java-hackerrank/Advanced/Java Singleton Pattern/Solution.java
@@ -0,0 +1,15 @@
+class Singleton {
+    public String str;
+
+    private Singleton() {}
+
+    private static Singleton instance;
+
+    public static Singleton getSingleInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+*/This is by Rusty*/
